7e32ff3bb840531b23a9115e6bc625ec6b67ceee
Amiri Hayes <89553495+AmiriHayes@users.noreply.github.com>
2023-04-09 20:33:39 -0400

    Update context.js

--- client/src/context.js
+++ client/src/context.js
@@ -9,9 +9,8 @@ const AppProvider = ({ children }) => {
   let [numQuotes, setNumQuotes] = useState(0)
 
   useEffect(() => {
-//     console.log('got here')
     const fetchData = async () => {
-      const reponse = await fetch('https://quote-app-c601.onrender.com/api').catch((err) => console.log(err))
+      const reponse = await fetch('https://quote-app-c601.onrender.com/api')
       const data = await reponse.json()
       setBackendData(data.quoteList)
       setNumQuotes(data.quoteList.length)
